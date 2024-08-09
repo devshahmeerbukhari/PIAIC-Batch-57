918748c27ca170fc7da206641d973e53a2a00a18
Muhammad Shahmeer Bukhari <shahmeerbukhari13@gmail.com>
2024-08-09 21:53:25 +0500

    Vercel 1st Project Deployee

---
+++ PIAIC-Batch-57-Q2-Generative-AI/Vercel Deployee/my-first-vercel-deployement/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function HomePage() {
+  return <>
+
+      <h1>Hello, I am Muhammad Shahmeer Bukahri and this is my first Next.js project.</h1>
+  </>
+
+}

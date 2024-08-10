71212a429ba0e3add5c89254f5b0bb66664e8dc7
Muhammad Shahmeer Bukhari <shahmeerbukhari13@gmail.com>
2024-08-10 23:29:08 +0500

    4th August Tailwing CSS Stylling Deployee

--- PIAIC-Batch-57-Q2-Generative-AI/4th August Tailwind Stylling/tailwind-stylling/src/app/blog/page.tsx
+++ PIAIC-Batch-57-Q2-Generative-AI/4th August Tailwind Stylling/tailwind-stylling/src/app/blog/page.tsx
@@ -4,7 +4,7 @@ function blog() {
   return (
     <h1>
         Hello This is the blog post
-        <p className='custom-container'>This is a paragraph tag with custom container stylling</p>
+        <p className='custom-container hover:bg-black hover:text-red-50 cursor-pointer'>This is a paragraph tag with custom container stylling, also hover on me</p>
     </h1>
   )
 }

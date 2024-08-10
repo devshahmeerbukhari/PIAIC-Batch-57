17d7476ad48a7a521c19d2782041be492c772ac9
Muhammad Shahmeer Bukhari <shahmeerbukhari13@gmail.com>
2024-08-10 22:34:11 +0500

    4th Aug 2024 Tailwind Stylling deployee

---
+++ PIAIC-Batch-57-Q2-Generative-AI/4th August Tailwind Stylling/tailwind-stylling/src/app/blog/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function blog() {
+  return (
+    <h1>
+        Hello This is the blog post
+        <p className='custom-container'>This is a paragraph tag with custom container stylling</p>
+    </h1>
+  )
+}
+
+export default blog
